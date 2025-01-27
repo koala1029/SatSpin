74e2e9fec66a00b840f2714957fb1596f6d92c93
koala1029 <koala1201au@gmail.com>
2025-01-27 23:51:01 +0900

    add the mobile menu

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -6,6 +6,12 @@ import CustomIconButton from "../Buttons/IconButton";
 import sound from "@/assets/icons/sound.png";
 import go from "@/assets/icons/go.png";
 import menu_icon from "@/assets/mobile/menu.png";
+import { useState } from "react";
+import home from "@/assets/icons/SideBar/home.png";
+import games from "@/assets/icons/SideBar/games.png";
+import history from "@/assets/icons/SideBar/history.png";
+import settings from "@/assets/icons/SideBar/settings.png";
+import x from "@/assets/images/X.png";
 
 interface HeaderProps {
   onDeposit: () => void;
@@ -14,7 +20,7 @@ interface HeaderProps {
 
 const Header = ({ onDeposit, onConnect }: HeaderProps) => {
   const navigate = useNavigate();
-
+  const [toggleMenu, setToogleMenu] = useState(true);
   return (
     <div className="fixed left-0 right-0 top-0 z-20 border-b border-b-borderColor1  bg-darkGray">
       <div className="flex h-[118px] items-center justify-between px-2 pb-7 pt-14 sm:px-4 md:px-8 lg:px-12 lg:py-7">
@@ -25,7 +31,7 @@ const Header = ({ onDeposit, onConnect }: HeaderProps) => {
           >
             <img src={logo} width={175} />
           </div>
-          <div className="block lg:hidden">
+          <div className="block lg:hidden  transition-all duration-300 ease-in-out hover:brightness-150 hover:drop-shadow-[0_0_10px_#fff]" onClick={() => { setToogleMenu(true) }}>
             <img src={menu_icon} />
           </div>
         </div>
@@ -47,16 +53,55 @@ const Header = ({ onDeposit, onConnect }: HeaderProps) => {
             icon={go}
             size={20}
             customClasses="hidden lg:block"
-            onClick={() => {}}
+            onClick={() => { }}
           ></CustomIconButton>
           <CustomIconButton
             icon={sound}
             size={20}
             customClasses="hidden lg:block"
-            onClick={() => {}}
+            onClick={() => { }}
           ></CustomIconButton>
         </div>
       </div>
+      {toggleMenu && (
+        <div
+          className="absolute top-0 lg:hidden flex flex-col shadow-md bg-black p-10 sm:p-16 gap-5 z-10 w-full h-[100vh] right-0 items-left text-xl"
+        >
+          <div className="flex gap-8 items-center py-1 sm:py-4 px-2 cursor-pointer" onClick={() => { setToogleMenu(false) }}>
+            <img src={x} className="w-[25px]  transition-all duration-300 ease-in-out hover:brightness-150 hover:drop-shadow-[0_0_10px_#fff]" />
+          </div>
+          <div className="w-full h-[1px] bg-borderColor1" />
+          <div onClick={() => { setToogleMenu(false); navigate("/") }} className="cursor-pointer flex gap-8 items-center py-1 sm:py-4 px-2  transition-all duration-300 ease-in-out hover:brightness-150 hover:drop-shadow-[0_0_10px_#fff]">
+            <div className="">
+              <img src={home} className="w-[30px]" />
+            </div>
+            <div className="">Home</div>
+          </div>
+          <div className="w-full h-[1px] bg-borderColor1"></div>
+          <div onClick={() => { setToogleMenu(false); navigate("/") }} className="cursor-pointer flex gap-8 items-center py-1 sm:py-4 px-2  transition-all duration-300 ease-in-out hover:brightness-150 hover:drop-shadow-[0_0_10px_#fff]">
+            <div className="">
+              <img src={games} className="w-[30px]" />
+            </div>
+            <div className="">Games</div>
+          </div>
+          <div className="w-full h-[1px] bg-borderColor1"></div>
+          <div onClick={() => { setToogleMenu(false); navigate("/leaderboard") }} className="cursor-pointer flex gap-8 items-center py-1 sm:py-4 px-2  transition-all duration-300 ease-in-out hover:brightness-150 hover:drop-shadow-[0_0_10px_#fff]">
+            <div className="">
+              <img src={history} className="w-[30px]" />
+            </div>
+            <div className="">Leaderboard</div>
+          </div>
+          <div className="w-full h-[1px] bg-borderColor1"></div>
+          <div onClick={() => { setToogleMenu(false); navigate("/setting") }} className="cursor-pointer flex gap-8 items-center py-1 sm:py-4 px-2  transition-all duration-300 ease-in-out hover:brightness-150 hover:drop-shadow-[0_0_10px_#fff]">
+            <div className="">
+              <img src={settings} className="w-[30px]" />
+            </div>
+            <div className="">Settings</div>
+          </div>
+          {/* <div className="w-full h-[1px] bg-borderColor1"></div> */}
+        </div>
+
+      )}
     </div>
   );
 };
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -4,17 +4,21 @@ import mobile_banner from "@/assets/mobile/banner.png";
 import FlowBox from "@/components/Boxs/FlowBox";
 import BaseFooter from "@/components/Layout/BaseFooter";
 import Games from "@/components/Layout/Games";
+import { useState } from "react";
 
 export default function Home() {
   return (
     <div className="pl-0 pt-[118px] font-space lg:pl-[50px]">
       <div className="w-[100%]">
         <img src={banner} className="hidden w-full lg:block"></img>
-        <img src={mobile_banner} className="block w-full lg:hidden"></img>
+        <div>
+          <img src={mobile_banner} className="block w-full lg:hidden"></img>
+        </div>
         <FlowBox />
         <Games />
         <BaseFooter />
       </div>
+
     </div>
   );
 }
