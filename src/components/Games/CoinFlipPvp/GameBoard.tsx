052acc881d1becd566672f1411e8a4e3d28390a0
koala1029 <koala1201au@gmail.com>
2025-01-27 17:57:19 +0900

    update the coinflip pvp mobile design

--- src/components/Games/CoinFlipPvp/GameBoard.tsx
+++ src/components/Games/CoinFlipPvp/GameBoard.tsx
@@ -14,40 +14,42 @@ const GameBoard = () => {
     <div className="relative mt-14 flex 2xl:flex-row flex-col justify-between overflow-hidden rounded-lg font-space text-xl pb-14">
       <PlayerBar
         title="PLAYERS"
-        customClasses="order-1"
+        customClasses="2xl:order-0 order-1"
         width="w-full"
         bar_height="560px"
       />
-      <div className="w-full  p-4 bg-black order-0">
-        <div className="grid gap-4 grid-cols-1 2xl:grid-cols-3 gap-8">
-          <div className="border-[3px] border-bgColor39 rounded-lg order-3">
+      <div className="w-full  p-4 bg-black 2xl:order-1 order-0">
+        <div className="grid gap-8 grid-cols-1 2xl:grid-cols-3">
+          <div className="border-[3px] border-bgColor39 rounded-lg order-4 2xl:order-1">
             <CreateGameItem />
           </div>
-          <div className="hidden 2xl:block">
+          <div className="hidden 2xl:block order-6 2xl:order-2">
             <GameRound />
           </div>
-          <div className="border-[3px] border-bgColor39 rounded-lg order-0">
+          <div className="border-[3px] border-bgColor39 rounded-lg order-1 2xl:order-3">
             <WinGameRound />
           </div>
-          <div className="order-4">
+          <div className="order-5 2xl:order-4">
             <JoinGameItem />
           </div>
-          <div className="order-1">
+          <div className="order-2 2xl:order-5">
             <GameRound />
           </div>
-          <div className="order-2">
+          <div className="order-3 2xl:order-6">
             <GameRound />
           </div>
-          <div className="hidden 2xl:block">
+          <div className="hidden 2xl:block order-7 2xl:order-7">
             <GameRound />
           </div>
-          <div className="hidden 2xl:block">
+          <div className="hidden 2xl:block order-8 2xl:order-8">
             <GameRound />
           </div>
-          <div className="hidden 2xl:block ">
+          <div className="hidden 2xl:block order-9 2xl:order-9">
             <BlurGameRound />
           </div>
         </div>
+
+
       </div>
     </div>
   );
